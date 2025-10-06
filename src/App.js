149843e6b18978e0dd7992a79fc07bb693fe7568
aneeshkp246 <aneeshkp1515@gmail.com>
2025-10-06 21:13:03 +0530

    Jenkinsfile changed

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy... Welcome!
+          Learn Jenkins on Udemy... Hello!
         </a>
       </header>
       <p>
